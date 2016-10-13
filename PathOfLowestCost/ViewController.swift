0e15e49ac389579490960872b0bc0a44fab5a2cd
Sheila Wallace <sheila@appsincorporated.com>
2016-10-13 15:22:23 -0400

    Added tests for ensuring UI elements are set, and code to set them based on the results of the calculator

--- PathOfLowestCost/ViewController.swift
+++ PathOfLowestCost/ViewController.swift
@@ -22,14 +22,62 @@ class ViewController: UIViewController {
     let passableString = "Yes"
     let notPassableString = "No"
     
+    let dataSet2 = [[3, 4, 1, 2, 8, 6], [6, 1, 8, 2, 7, 4], [5, 9, 3, 9, 9, 5], [8, 4, 1, 3, 2, 6], [3, 7, 2, 1, 2, 3]]
+    let dataSet3 = [[19, 10, 19, 10, 19], [21, 23, 20, 19, 12], [20, 12, 20, 11, 10]]
+    let dataSet4 = [[], [], [], [], []]
+    
+    
     @IBAction func dataSet1Selected() {
+        let dataSet =
+            [[3, 4, 1, 2, 8, 6],
+             [6, 1, 8, 2, 7, 4],
+             [5, 9, 3, 9, 9, 5],
+             [8, 4, 1, 3, 2, 6],
+             [3, 7, 2, 8, 6, 4]]
+        calculateResultsForDataSet(dataSet: dataSet)
+        dataSet1Button.isSelected = true
+        dataSet2Button.isSelected = false
+        dataSet3Button.isSelected = false
     }
     
     @IBAction func dataSet2Selected() {
-   }
-    
-    @IBAction func dataSet3Selected() {
+        let dataSet =
+            [[3, 4, 1, 2, 8, 6],
+             [6, 1, 8, 2, 7, 4],
+             [5, 9, 3, 9, 9, 5],
+             [8, 4, 1, 3, 2, 6],
+             [3, 7, 2, 1, 2, 3]]
+        calculateResultsForDataSet(dataSet: dataSet)
+        dataSet1Button.isSelected = false
+        dataSet2Button.isSelected = true
+        dataSet3Button.isSelected = false
     }
     
+    @IBAction func dataSet3Selected() {
+        let dataSet =
+            [[19, 10, 19, 10, 19],
+             [21, 23, 20, 19, 12],
+             [20, 12, 20, 11, 10]]
+        calculateResultsForDataSet(dataSet: dataSet)
+        dataSet1Button.isSelected = false
+        dataSet2Button.isSelected = false
+        dataSet3Button.isSelected = true
+    }
+    
+    func calculateResultsForDataSet(dataSet: [[Int]]) {
+        let result = calculator.calculateLowestCost(dataSet: dataSet)
+        
+        passableResultLabel!.text = result.isPathPassable ? passableString : notPassableString
+        costResultLabel!.text = "\(result.lowestCost)"
+        setPathResultLabel(path: result.path)
+    }
+    
+    func setPathResultLabel(path: [Int]) {
+        var pathString = ""
+        for pathValue in path {
+            pathString += "\(pathValue)" + " "
+        }
+        pathResultLabel!.text = pathString
+    }    
 }
 
--- PathOfLowestCostTests/ViewControllerTests.swift
+++ PathOfLowestCostTests/ViewControllerTests.swift
@@ -42,6 +42,51 @@ class ViewControllerTests: XCTestCase {
         XCTAssertNotNil(viewController!.dataSet3Button)
     }
     
+    func testDataSet1SelectedSetsPassableResultLabel() {
+        viewController!.dataSet1Selected()
+        XCTAssertEqual(viewController!.passableResultLabel!.text!, viewController!.passableString)
+    }
+    
+    func testDataSet2SelectedSetsPassableResultLabel() {
+        viewController!.dataSet2Selected()
+        XCTAssertEqual(viewController!.passableResultLabel!.text!, viewController!.passableString)
+    }
+    
+    func testDataSet3SelectedSetsPassableResultLabel() {
+        viewController!.dataSet3Selected()
+        XCTAssertEqual(viewController!.passableResultLabel!.text!, viewController!.notPassableString)
+    }
+
+    func testDataSet1SelectedSetsCostResultLabel() {
+        viewController!.dataSet1Selected()
+        XCTAssertEqual(viewController!.costResultLabel!.text!, "16")
+    }
+    
+    func testDataSet2SelectedSetsCostResultLabel() {
+        viewController!.dataSet2Selected()
+        XCTAssertEqual(viewController!.costResultLabel!.text!, "11")
+    }
+    
+    func testDataSet3SelectedSetsCostResultLabel() {
+        viewController!.dataSet3Selected()
+        XCTAssertEqual(viewController!.costResultLabel!.text!, "48")
+    }
+    
+    func testDataSet1SelectedSetsPathResultLabel() {
+        viewController!.dataSet1Selected()
+        XCTAssertEqual(viewController!.pathResultLabel!.text!, "1 2 3 4 4 5 ")
+    }
+    
+    func testDataSet2SelectedSetsPathResultLabel() {
+        viewController!.dataSet2Selected()
+        XCTAssertEqual(viewController!.pathResultLabel!.text!, "1 2 1 5 5 5 ")
+    }
+    
+    func testDataSet3SelectedSetsPathResultLabel() {
+        viewController!.dataSet3Selected()
+        XCTAssertEqual(viewController!.pathResultLabel!.text!, "1 1 1 ")
+    }
+    
     func setupFromStoryboard() {
         viewController = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ViewController") as? ViewController
         viewController!.loadView()
